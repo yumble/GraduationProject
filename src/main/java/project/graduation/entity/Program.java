7fef090db0d46f45d8487b892da999f858249591
yumble <hnjn1101@gmail.com>
2023-04-17 17:42:14 +0900

    feat: Program Entity 생성
    - todo : TB_COLLECT_LOCATION 엔티티에 연관관계 매핑필요

---
+++ src/main/java/project/graduation/entity/Program.java
@@ -0,0 +1,53 @@
+package project.graduation.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "TB_PROGRAM_INFO")
+public class Program implements Persistable<String> {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String programId;
+    @NotNull
+    private Integer priority;
+    @NotNull
+    private String type;
+    private String commandPath;
+    private String arguments;
+    @CreatedBy
+    @Column(name = "CREATED_DATE", columnDefinition = "timestamp default CURRENT_TIMESTAMP not null")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @Override
+    public String getId() {
+        return programId;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+
+}
