d03f00ecfa4a31c41b47ee0d056897e695d8d253
yumble <hnjn1101@gmail.com>
2023-04-21 00:21:49 +0900

    feat: GPSDto 생성
    - 위도, 경도, 고도, 건물층수를 담은 객체

---
+++ src/main/java/project/graduation/dto/GPSDto.java
@@ -0,0 +1,11 @@
+package project.graduation.dto;
+
+import lombok.Data;
+
+@Data
+public class GPSDto {
+    private String latitude;
+    private String longitude;
+    private String altitude;
+    private String floor;
+}
