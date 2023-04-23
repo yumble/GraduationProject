84e052ff55f539188bd8f02142cb2ea2dc8964cb
yumble <hnjn1101@gmail.com>
2023-04-23 10:03:04 +0900

    feat : CollectController 생성 및 Dto 생성

---
+++ src/main/java/project/graduation/controller/CollectController.java
@@ -0,0 +1,38 @@
+package project.graduation.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import project.graduation.config.resultform.ResultException;
+import project.graduation.config.resultform.ResultResponse;
+import project.graduation.dto.AddressDto;
+import project.graduation.dto.GPSDto;
+import project.graduation.entity.GeneralFile;
+import project.graduation.service.GeneralFileService;
+
+import static project.graduation.config.resultform.ResultResponseStatus.REQUEST_ERROR;
+
+@Slf4j
+@RequestMapping("/lidar")
+@RestController
+@RequiredArgsConstructor
+public class CollectController {
+    private final CollectService collectService;
+
+    @PostMapping
+    public ResultResponse<GeneralFile> saveFile(@RequestPart @Valid AddressDto address,
+                                                @RequestPart @Valid GPSDto location,
+                                                @RequestPart MultipartFile file,
+                                                BindingResult br) {
+        if (br.hasErrors()) {
+            throw new ResultException(REQUEST_ERROR);
+        }
+
+        collectService.saveFile(address, location, file);
+
+        return new ResultResponse<>(generalFile, null);
+    }
+}ㅡ
