a2fcbb857dfd91c42a626a057ff08af087e7fd52
yumble <hnjn1101@gmail.com>
2023-05-01 20:12:47 +0900

    feat: 건물 리스트 출력 API Related to #9
    - AddressController : getBuildings Function
    - AddressService : getBuildings Function, Paging 기능 추가
    - AddressRepository : findAll()
    - AddressDto : Address Entity 받아서 AddressDto 객체 생성하는 생성자추가

---
+++ src/main/java/project/graduation/service/AddressService.java
@@ -0,0 +1,26 @@
+package project.graduation.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.graduation.dto.AddressDto;
+import project.graduation.repository.AddressRepository;
+
+@Transactional(readOnly = true)
+@Slf4j
+@Service
+@AllArgsConstructor
+public class AddressService {
+    private final AddressRepository addressRepository;
+
+    public Page<AddressDto> getBuildings() {
+        PageRequest pageable = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "createdDate"));
+        return addressRepository.findAll(pageable)
+                .map(AddressDto::new);
+    }
+
+}
